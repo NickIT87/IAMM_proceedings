75a0f33549ef127526d352845a177e9a27354bef
NickIT87 <elanir358@gmail.com>
2024-02-06 13:27:32 +0200

    bt form with e messages

--- Linguistic_representation_of_deterministic_graphs/web.py
+++ Linguistic_representation_of_deterministic_graphs/web.py
@@ -12,40 +12,69 @@ import networkx as nx
 from AlgorithmsLibraries.alglib_version_02_current import ap_graph, ac_pair
 
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+#external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+#app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-app.layout = html.Div([
-    html.Br(),
-    html.H4('Linguistic representation of D-graph', style={'textAlign': 'center'}),
-    html.Br(),
-    html.I('Root label: ', style={'padding': '10px'}),
-    dcc.Input(id='root_data', type='text', value='1', maxLength=1, style={
-        'border': '2px solid #ddd',
-        'padding': '10px',
-        'width': '35px'
-    }),
-    html.I('C component: ', style={'padding': '10px'}),
-    dcc.Input(id='c_data', type='text', placeholder="type C word", style={
-        'border': '2px solid #ddd',
-        'padding': '10px',
-        'width': '300px'
-    }),
-    html.I('L component: ', style={'padding': '10px'}),
-    dcc.Input(id='l_data', type='text', placeholder="type L word", style={
-        'border': '2px solid #ddd',
-        'padding': '10px',
-        'width': '300px'
-    }),
-    dbc.Button("Submit", id='submit-button', n_clicks=0, color="primary", style={'marginLeft': '10px'}),
-    html.Br(),
-    dcc.Graph(id='network-graph', style={'marginTop': '10px'}),
-    html.Br(),
-    html.H4('graph metrics: ', style={'textAlign': 'center'}),
-    html.Br(),
-    html.Div(id='error-message', style={'color': 'red'}),
-    dcc.Textarea(id='output-text', value='', style={'width': '100%', 'height': 200}),
-])
+root_input = html.Div(
+    [
+        dbc.Label("Root label: ", html_for="root_data"),
+        dbc.Input(type="text", id="root_data", value='0', maxLength=1 , style={"width": "36px"}),
+        dbc.FormText(
+            "root label must be a char: '1' or 'A' etc ... ",
+            color="secondary",
+        ),
+    ],
+    className="mb-3",
+)
+
+c_input = html.Div(
+    [
+        dbc.Label("C component: ", html_for="c_data"),
+        dbc.Input(type="text", id="c_data", placeholder="Enter words that described graph cycles"),
+        dbc.FormText(
+            "Enter words that described graph cycles",
+            color="secondary",
+        ),
+    ],
+    className="mb-3",
+)
+
+l_input = html.Div(
+    [
+        dbc.Label("L component: ", html_for="l_data"),
+        dbc.Input(type="text", id="l_data", placeholder="Enter words that described leaf nodes"),
+        dbc.FormText(
+            "Enter words that described leaf nodes",
+            color="secondary",
+        ),
+    ],
+    className="mb-3",
+)
+
+submit_btn = html.Div(
+    [
+        dbc.Button("Submit", id='submit-button', n_clicks=0, color="primary")
+    ]
+)
+
+form = dbc.Form([root_input, c_input, l_input, submit_btn])
+
+app.layout = dbc.Container(
+    html.Div([
+        html.Br(),
+        html.H4('Linguistic representation of D-graph', style={'textAlign': 'center'}),
+        html.Br(),
+        form,
+        html.Br(),
+        html.Div(id='error-message', style={'color': 'red'}),
+        dcc.Graph(id='network-graph', style={'marginTop': '10px'}),
+        html.Br(),
+        html.H4('graph metrics: ', style={'textAlign': 'center'}),
+        html.Br(),
+        dcc.Textarea(id='output-text', value='', style={'width': '100%', 'height': 200}),
+    ])
+)
 
 
 @app.callback(
@@ -76,7 +105,7 @@ def update_graph(n_clicks, c_val, l_val, root_val):
         G = ap_graph(c_component, l_component, root_val)
     except ValueError as e:
         # Display error message to the user
-        return dash.no_update, '', f'Error: {str(e)}. Please check your input.'
+        return dash.no_update, '', f'Error: {str(e)} Please check your input.'
 
     # Create Plotly figure from NetworkX graph
     pos = nx.spring_layout(G)  # Layout algorithm (e.g., spring_layout, circular_layout)
