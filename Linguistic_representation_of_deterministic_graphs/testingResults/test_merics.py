ac26065068c0e2745b651c90479295152c0f7fca
NickIT87 <elanir358@gmail.com>
2024-11-23 22:18:18 +0200

    preproduction code branch

--- Linguistic_representation_of_deterministic_graphs/testingResults/test_merics.py
+++ Linguistic_representation_of_deterministic_graphs/testingResults/test_merics.py
@@ -12,6 +12,23 @@ def custom_cc_rank(complexity):
     else:
         return 'C'  # For complexity > 20, we keep it as 'C'
 
+def halstead_report_to_dict(report):
+    """Converts a HalsteadReport object into a dictionary."""
+    return {
+        "h1": report.h1,
+        "h2": report.h2,
+        "N1": report.N1,
+        "N2": report.N2,
+        "vocabulary": report.vocabulary,
+        "length": report.length,
+        "calculated_length": report.calculated_length,
+        "volume": report.volume,
+        "difficulty": report.difficulty,
+        "effort": report.effort,
+        "time": report.time,
+        "bugs": report.bugs,
+    }
+
 
 def analyze_code(file_path):
     with open(file_path, 'r') as file:
@@ -34,14 +51,24 @@ def analyze_code(file_path):
     # Cyclomatic Complexity results
     print(f'Cyclomatic Complexity (CC):')
     for cr in cc_results:
-        print(f'  - Function: {cr.name}, Complexity: {cr.complexity}')
+        print(f'  - Function: {cr.name}, Complexity: {cr.complexity} {custom_cc_rank(cr.complexity)}')
     print(f'Total Cyclomatic Complexity: {total_complexity}')
     print(f'Cyclomatic Complexity Rank: {cc_rank_value}')
 
     # Maintainability Index results
-    print(f'\nMaintainability Index (MI): {mi_score}')
+    print(f'Maintainability Index (MI): {mi_score}\n')
 
-    #print(halstead_results)
+    a = list(halstead_results)
+    print("Total:\n", halstead_report_to_dict(a[0]))
+    print("\nHalsted metric by functions:")
+    for i in a[1]:
+        print("\n", i[0])
+        print(halstead_report_to_dict(i[1]))
+
+
 # Specify the path to your Python file
 file_path = "../AlgorithmsLibraries/alglib_prod_version_1_0_0.py"
 analyze_code(file_path)
+
+# file_path = "../AlgorithmsLibraries/alglib_version_01_legacy.py"
+# analyze_code(file_path)
